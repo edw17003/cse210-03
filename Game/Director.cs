0fe1f856d985db4ef004314e54cb2fa0286071d0
JoshJ-WDD <joh21036@byui.edu>
2022-05-18 11:15:01 -0600

    CLEAN SLATE - removed comments and most code

--- Game/Director.cs
+++ Game/Director.cs
@@ -1,11 +1,6 @@
 namespace Unit03.Game
 {
-    /// <summary>
-    /// <para>A person who directs the game.</para>
-    /// <para>
-    /// The responsibility of a Director is to control the sequence of play.
-    /// </para>
-    /// </summary>
+
     public class Director
     {
         private Jumper Jumper = new Jumper();
@@ -13,16 +8,12 @@ namespace Unit03.Game
         private Player Player = new Player();
         private IO IO = new IO();
 
-        /// <summary>
-        /// Constructs a new instance of Director.
-        /// </summary>
+
         public Director()
         {
         }
 
-        /// <summary>
-        /// Starts the game by running the main game loop.
-        /// </summary>
+
         public void StartGame()
         {
             while (isPlaying)
@@ -33,35 +24,21 @@ namespace Unit03.Game
             }
         }
 
-        /// <summary>
-        /// Moves the Player to a new location.
-        /// </summary>
+
         private void GetInputs()
         {
-            IO.WriteText(Jumper.location.ToString());
-            int location = IO.ReadNumber("\nEnter a location [1-1000]: ");
-            Player.MoveLocation(location);
+
         }
 
-        /// <summary>
-        /// Keeps watch on where the Player is moving.
-        /// </summary>
+
         private void DoUpdates()
         {
-            Jumper.WatchPlayer(Player);
+
         }
 
-        /// <summary>
-        /// Provides a hint for the Player to use.
-        /// </summary>
+
         private void DoOutputs()
         {
-            string hint = Jumper.GetHint();
-            IO.WriteText(hint);
-            if (Jumper.IsFound())
-            {
-                isPlaying = false;
-            }
             
         }
     }
--- Game/IO.cs
+++ Game/IO.cs
@@ -1,50 +1,22 @@
 using System;
 
-
 namespace Unit03.Game
 {
-    /// <summary>
-    /// <para>A service that handles terminal operations.</para>
-    /// <para>
-    /// The responsibility of a IO is to provide input and output operations for the 
-    /// terminal.
-    /// </para>
-    /// </summary>
+
     public class IO
     {
-        /// <summary>
-        /// Constructs a new instance of IO.
-        /// </summary>
+
         public IO()
         {
         }
 
-        /// <summary>
-        /// Gets numerical input from the terminal. Directs the user with the given prompt.
-        /// </summary>
-        /// <param name="prompt">The given prompt.</param>
-        /// <returns>Inputted number.</returns>
-        public int ReadNumber(string prompt)
-        {
-            string rawValue = ReadText(prompt);
-            return int.Parse(rawValue, System.Globalization.CultureInfo.InvariantCulture);
-        }
 
-        /// <summary>
-        /// Gets text input from the terminal. Directs the user with the given prompt.
-        /// </summary>
-        /// <param name="prompt">The given prompt.</param>
-        /// <returns>Inputted text.</returns>
         public string ReadText(string prompt)
         {
             Console.Write(prompt);
             return Console.ReadLine();
         }
 
-        /// <summary>
-        /// Displays the given text on the terminal. 
-        /// </summary>
-        /// <param name="text">The given text.</param>
         public void WriteText(string text)
         {
             Console.WriteLine(text);
