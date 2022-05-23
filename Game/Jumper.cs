30f2ad8a25ed3f953133ff412ed087ccb8945b91
prattbj <104381242+prattbj@users.noreply.github.com>
2022-05-23 11:23:49 -0600

    fixed format

--- Game/Jumper.cs
+++ Game/Jumper.cs
@@ -8,7 +8,7 @@ namespace Unit03.Game
     {
         //string that holds an array of parachute values
         private string[] parachute = {"  ___   ", " /___\\  ", " \\   /  ", "  \\ /"};
-        private string[] jumper = {"  o", "  /|\\", " /  \\"};
+        private string[] jumper = {"  o", " /|\\", " /  \\"};
         //constructor
         public Jumper() 
         {
