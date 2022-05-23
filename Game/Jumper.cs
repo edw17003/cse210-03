a44902031888d506a7dfa95afce82be7ac3382cf
prattbj <104381242+prattbj@users.noreply.github.com>
2022-05-23 10:42:39 -0600

    parachute methods made

--- Game/Jumper.cs
+++ Game/Jumper.cs
@@ -1,29 +1,24 @@
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 
 namespace Unit03.Game 
 {
     public class Jumper
     {
-        public Jumper()
+        //string that holds an array of parachute values
+        private string[] parachute = {"  ___   ", " /___\\  ", " \\   /  ", "  \\ /"};
+        //constructor
+        public Jumper() {}
+        //returns the parachute
+        public string[] getParachute()
         {
-
+            return this.parachute;
         }
-
-        public string[] Parachute()
+        //breaks (removes) the parachute from top down
+        public void breakParachute()
         {
-            string[] parachute = {"  ___   ", " /___\\  ", " \\   /  ", "  \\ /",};
-            return parachute;
-        }
-
-        // public string[] BreakParachute(string[] parachute)
-        // {
-        //     parachute.Remove[0];
-        // }
-
-        public string NewWord() {
-            return "Check Jumper.NewWord()";
+            this.parachute = parachute.Skip(1).ToArray();
         }
     }
 }
